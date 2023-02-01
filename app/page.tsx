8c9a0043b080630f78619a72dabbbac91c4cf93d
Miguel Hernanz <migherdcas@gmail.com>
2023-02-01 23:33:31 +0800

    fetch posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
-export default function Home() {
+async function getPosts() {
+  const response = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!response.ok) {
+    console.error(response);
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  console.log(posts);
+
   return (
     <main className="p-8">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
